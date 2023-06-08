221739303a300b0986a85fdaa27a16d5ac8995f7
mrjones-plip <mrjones-plip@plip.com>
2023-06-08 16:32:18 -0700

    simplify DNS script to bare min

--- .github/scripts/dns.sh
+++ .github/scripts/dns.sh
@@ -2,89 +2,24 @@
 
 echo
 echo "------------------------"
-echo "running: dig local-ip.medicmobile.org +short"
+echo "calling: resolvectl dns eth0"
+resolvectl dns eth0
+
+echo
+echo "------------------------"
+echo "calling: dig local-ip.medicmobile.org  +short"
 dig local-ip.medicmobile.org +short
 
 echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org +short @9.9.9.9"
-dig local-ip.medicmobile.org +short @9.9.9.9
+echo "calling: sudo resolvectl dns eth0 9.9.9.9 149.112.112.112"
+sudo resolvectl dns eth0 9.9.9.9 149.112.112.112
 
 echo
 echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +short @8.8.8.8"
-dig local-ip.medicmobile.org  +short @8.8.8.8
-
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +short @168.63.129.16"
-dig local-ip.medicmobile.org  +short @168.63.129.16
-
+echo "calling: resolvectl dns eth0"
+resolvectl dns eth0
 
 echo
 echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +trace @168.63.129.16"
-dig local-ip.medicmobile.org  +short @168.63.129.16
-
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +trace"
-dig local-ip.medicmobile.org  +short
-
-
-#echo
-#echo "------------------------"
-#echo "installing network-manager "
-#sudo apt update
-#sudo apt install network-manager
-
-
-echo
-echo "------------------------"
-echo "calling: resolvectl status eth0"
-resolvectl status eth0
-
-echo
-echo "------------------------"
-echo "calling: sudo resolvectl dns eth0 9.9.9.9"
-sudo resolvectl dns eth0 9.9.9.9
-
-echo
-echo "------------------------"
-echo "calling: resolvectl status eth0"
-resolvectl status eth0
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org +short"
+echo "calling: dig local-ip.medicmobile.org  +short"
 dig local-ip.medicmobile.org +short
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org +short @9.9.9.9"
-dig local-ip.medicmobile.org +short @9.9.9.9
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +short @8.8.8.8"
-dig local-ip.medicmobile.org  +short @8.8.8.8
-
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +short @168.63.129.16"
-dig local-ip.medicmobile.org  +short @168.63.129.16
-
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +trace @168.63.129.16"
-dig local-ip.medicmobile.org  +short @168.63.129.16
-
-
-echo
-echo "------------------------"
-echo "running: dig local-ip.medicmobile.org  +trace"
-dig local-ip.medicmobile.org  +short
