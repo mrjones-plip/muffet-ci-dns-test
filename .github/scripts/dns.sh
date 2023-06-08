a9c5d2bc7e5061da1e65c701d8b9d55df58f8fe6
mrjones-plip <mrjones-plip@plip.com>
2023-06-08 10:50:51 -0700

    add nicer debug output, install network-manager for nmcli

--- .github/scripts/dns.sh
+++ .github/scripts/dns.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org +short"
+dig local-ip.medicmobile.org +short
 
-dig local-ip.medicmobile.org
-ip a
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org +short @9.9.9.9"
+dig local-ip.medicmobile.org +short @9.9.9.9
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org  +short @8.8.8.8"
+dig local-ip.medicmobile.org  +short @8.8.8.8
+
+
+echo
+echo "------------------------"
+echo "installing network-manager and calling 'nmcli device show eth0 | grep DNS''"
+apt update
+apt install network-manager
 nmcli device status
 nmcli device show eth0 | grep DNS
