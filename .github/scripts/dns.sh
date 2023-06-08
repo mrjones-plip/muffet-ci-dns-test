de486adb7b486046d80b270c93ecf3a5ee1c49a7
mrjones-plip <mrjones-plip@plip.com>
2023-06-08 12:17:21 -0700

    remove unused debug, add trace

--- .github/scripts/dns.sh
+++ .github/scripts/dns.sh
@@ -18,33 +18,64 @@ dig local-ip.medicmobile.org  +short @8.8.8.8
 
 echo
 echo "------------------------"
-echo "installing network-manager "
-sudo apt update
-sudo apt install network-manager
-
-echo
-echo "------------------------"
-echo "calling: sudo nmcli device status"
-sudo nmcli device status
-
-echo
-echo "------------------------"
-echo "calling: sudo nmcli device show "
-sudo nmcli device show
-
-echo
-echo "------------------------"
-echo "calling: systemd-resolve --status "
-systemd-resolve --status
+echo "running: dig local-ip.medicmobile.org  +short @168.63.129.16"
+dig local-ip.medicmobile.org  +short @168.63.129.16
 
 
 echo
 echo "------------------------"
-echo "calling: resolvectl "
-resolvectl
+echo "running: dig local-ip.medicmobile.org  +trace @168.63.129.16"
+dig local-ip.medicmobile.org  +short @168.63.129.16
 
 
 echo
 echo "------------------------"
-echo "calling: cat /etc/resolv.conf "
-cat /etc/resolv.conf
+echo "running: dig local-ip.medicmobile.org  +trace"
+dig local-ip.medicmobile.org  +short
+
+
+#echo
+#echo "------------------------"
+#echo "installing network-manager "
+#sudo apt update
+#sudo apt install network-manager
+
+
+echo
+echo "------------------------"
+echo "calling: resolvectl eth0"
+resolvectl eth0
+
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org +short"
+dig local-ip.medicmobile.org +short
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org +short @9.9.9.9"
+dig local-ip.medicmobile.org +short @9.9.9.9
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org  +short @8.8.8.8"
+dig local-ip.medicmobile.org  +short @8.8.8.8
+
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org  +short @168.63.129.16"
+dig local-ip.medicmobile.org  +short @168.63.129.16
+
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org  +trace @168.63.129.16"
+dig local-ip.medicmobile.org  +short @168.63.129.16
+
+
+echo
+echo "------------------------"
+echo "running: dig local-ip.medicmobile.org  +trace"
+dig local-ip.medicmobile.org  +short
