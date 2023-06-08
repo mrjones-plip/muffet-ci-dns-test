7859a1c7acd58e5b0e7606b3056e6decb4d2d67a
mrjones-plip <mrjones-plip@plip.com>
2023-06-08 10:54:50 -0700

    more debug, more sudo sandwiching

--- .github/scripts/dns.sh
+++ .github/scripts/dns.sh
@@ -18,8 +18,16 @@ dig local-ip.medicmobile.org  +short @8.8.8.8
 
 echo
 echo "------------------------"
-echo "installing network-manager and calling 'nmcli device show eth0 | grep DNS''"
+echo "installing network-manager "
 sudo apt update
 sudo apt install network-manager
-nmcli device status
-nmcli device show eth0 | grep DNS
+
+echo
+echo "------------------------"
+echo "calling: sudo nmcli device status"
+sudo nmcli device status
+
+echo
+echo "------------------------"
+echo "calling: sudo nmcli device show eth0 | grep DNS"
+sudo nmcli device show eth0 | grep DNS
